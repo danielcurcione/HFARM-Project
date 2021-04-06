7e48dbc7754dd75c9baa83cbeeec43055f6bfc43
daniel.curcione <daniel.curcione@gmail.com>
2021-04-06 23:14:09 +0200

    Dist folder generation

---
+++ .nuxt/dist/server/components/add-dialog.js
@@ -0,0 +1,130 @@
+exports.ids = [1,4];
+exports.modules = {
+
+/***/ 22:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/Header.vue?vue&type=template&id=556cfa65&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_vm._ssrNode("<div class=\"content\"><nav class=\"level\"><div class=\"level-left\"><h1 class=\"title\">Calls</h1></div> <div class=\"level-right\"><div class=\"level-item\"><div class=\"field has-addons\"><p class=\"control\"><input type=\"text\" placeholder=\"Search...\" class=\"input\"></p> <p class=\"control\"><button class=\"button\">\n                Search\n              </button></p></div></div></div></nav></div>")])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./components/Header.vue?vue&type=template&id=556cfa65&
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./components/Header.vue
+
+var script = {}
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  script,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  "499c4cc0"
+  
+)
+
+/* harmony default export */ var Header = __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ 27:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/AddDialog.vue?vue&type=template&id=480f5996&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"modal is-active"},[_vm._ssrNode("<div class=\"modal-background\"></div> <div class=\"modal-card\"><header class=\"modal-card-head\"><p class=\"modal-card-title\">Aggiungi nuova</p> <button aria-label=\"close\" class=\"delete\"></button></header> <section class=\"modal-card-body\"><div class=\"field\"><label class=\"label\">Nome</label> <div class=\"control\"><input type=\"text\" placeholder=\"Text input\" class=\"input\"></div></div> <div class=\"field\"><label class=\"label\">Cliente</label> <div class=\"control\"><input type=\"text\" placeholder=\"Text input\" class=\"input\"></div></div> <div class=\"field\"><div class=\"control\"><label class=\"checkbox\"><input type=\"checkbox\">\n            Aggiungi ai preferiti\n          </label></div></div></section> <footer class=\"modal-card-foot\"><button class=\"button is-success\">Conferma</button> <button class=\"button\">Annulla</button></footer></div>")])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./components/AddDialog.vue?vue&type=template&id=480f5996&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/AddDialog.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var AddDialogvue_type_script_lang_js_ = ({});
+// CONCATENATED MODULE: ./components/AddDialog.vue?vue&type=script&lang=js&
+ /* harmony default export */ var components_AddDialogvue_type_script_lang_js_ = (AddDialogvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./components/AddDialog.vue
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  components_AddDialogvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  "7731daf4"
+  
+)
+
+/* harmony default export */ var AddDialog = __webpack_exports__["default"] = (component.exports);
+
+/* nuxt-component-imports */
+installComponents(component, {Header: __webpack_require__(22).default})
+
+
+/***/ })
+
+};;
+//# sourceMappingURL=add-dialog.js.map
---
+++ .nuxt/dist/server/components/header.js
@@ -0,0 +1,44 @@
+exports.ids = [4];
+exports.modules = {
+
+/***/ 22:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/Header.vue?vue&type=template&id=556cfa65&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_vm._ssrNode("<div class=\"content\"><nav class=\"level\"><div class=\"level-left\"><h1 class=\"title\">Calls</h1></div> <div class=\"level-right\"><div class=\"level-item\"><div class=\"field has-addons\"><p class=\"control\"><input type=\"text\" placeholder=\"Search...\" class=\"input\"></p> <p class=\"control\"><button class=\"button\">\n                Search\n              </button></p></div></div></div></nav></div>")])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./components/Header.vue?vue&type=template&id=556cfa65&
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./components/Header.vue
+
+var script = {}
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  script,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  "499c4cc0"
+  
+)
+
+/* harmony default export */ var Header = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=header.js.map
